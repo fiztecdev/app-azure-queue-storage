fe5b694446a530d0ea9cbfa9fed7ef74e5b2d00b
Fidel Villanueva <villanuevafidelid@gmail.com>
2021-07-31 12:37:17 -0500

    Init Commit - Azure Queue Storage

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+
+namespace QueueApp
+{
+    class Program
+    {
+        private const string ConnectionString = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=fiztecdev01;AccountKey=NT7voj70ikQbwdHDl8yTATdIx7vJh+0sa1mZsYtxu26GvsTnICckMrNQZPZTw8ftbR8ek6aHgzc9d01GOygUBQ==";
+        static async Task Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                string value = String.Join(" ", args);
+                await SendArticleAsync(value);
+                Console.WriteLine($"Sent: {value}");
+            }
+        }
+        static async Task SendArticleAsync(string newsMessage)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConnectionString);
+
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference("newsqueue");
+            bool createdQueue = await queue.CreateIfNotExistsAsync();
+            if (createdQueue)
+            {
+                Console.WriteLine("The queue of news articles was created.");
+            }
+
+            CloudQueueMessage articleMessage = new CloudQueueMessage(newsMessage);
+            await queue.AddMessageAsync(articleMessage);
+        }
+    }
+}
